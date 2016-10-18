a28ed62f1f9a9c2eeb4b0c1d7c15609b42059c17
AlxndrPsclt <alxndr.psclt@gmail.com>
2016-10-19 01:21:23 +0200

    Adds 2 python snippets for file reading and applying some function to every line of a file. Corrects a snippet for json saving.

---
+++ scripts/ccase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import inflection
+import sys
+
+#text=" ".join([sys.argv[1]])
+#
+#camelized_text = inflection.camelize(text)
+#
+#if text != camelized_text:
+#    print(camelized_text)
+#else:
+#    print(inflection.underscore(text))
+
+print(str(sys.argv))
+
